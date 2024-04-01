e5ca16510d047db50fa0a21c9da5463d807cd481
shwarz113 <shwarz13@yandex.ru>
2024-04-01 15:56:18 +0300

    update

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const SOCKET_URL = 'wss://tgame-crypto-coin-kagertanin.amvera.io/connect';
-export const DOMAIN = process.env.NODE_ENV === 'production' ? '/tgame/' : '/';
+export const DOMAIN = process.env.NODE_ENV === 'production' ? '/miner-game/' : '/';
 
 export enum PagesEnum {
     PEOPLE = 'people',
